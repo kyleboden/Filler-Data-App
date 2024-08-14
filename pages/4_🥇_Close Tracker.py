3b96c491da95f4ffa5209995e1fca7b695d24fa6
kyleboden <124191228+kyleboden@users.noreply.github.com>
2024-08-14 11:19:54 -0600

    Update 4_🥇_Close Tracker.py

--- pages/4_🥇_Close Tracker.py
+++ pages/4_🥇_Close Tracker.py
@@ -7,5 +7,5 @@ import streamlit as st
 # Page-specific code
 st.image("images/AMP_Logo.png", width=300)
 st.title("Close Tracker")
-google_sheet_url = "https://docs.google.com/spreadsheets/d/1idziebQqf5CVqqQ64IINvdJXXCZtL5N0Yf_UCAbXEGE/edit?gid=0#gid=0"
+google_sheet_url = "https://docs.google.com/spreadsheets/d/1ofC-iCsJ6p7HBAfgcQ6qzw8eQLaSU5tTa6OzYln7dO0/edit?gid=616940338#gid=616940338"
 st.write("Open the Google Sheet [here](%s)." % google_sheet_url)
